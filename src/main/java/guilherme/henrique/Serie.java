8f16f88c6de58131ba82269fa92a1d7273b3c063
guilherme <gdesouza@ies-eugeni.cat>
2024-08-25 21:01:31 +0200

    comitaio 1

---
+++ src/main/java/guilherme/henrique/Serie.java
@@ -0,0 +1,18 @@
+package guilherme.henrique;
+
+public class Serie {
+
+
+
+    private final static String dir=".data/";
+    private final static String fitxer = dir+"persones.csv";
+    private static Fitxers f = new Fitxers();
+
+    //<editor-fold desc="Propietats">
+    private int id;
+    private String nom;
+    private String cognom;
+    private int edat;
+    private double sou;
+
+}
